ab89a51314f897429e3676a1532b17e338e47013
rionando <rionandosoeksin@gmail.com>
2025-02-11 23:20:51 +0800

    feat: add login endpoint

--- kkn_backend/urls.py
+++ kkn_backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/auth/token', TokenObtainPairView.as_view(), name="get-token"),
+    path('api/auth/token/refresh', TokenRefreshView.as_view(), name="refresh-token"),
 ]
