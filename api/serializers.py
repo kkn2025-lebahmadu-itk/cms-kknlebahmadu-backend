8414326ba9f1b2bc7dc989afa120354fa7b16621
rionando <rionandosoeksin@gmail.com>
2025-02-19 15:35:21 +0800

    feat: news setup

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from data.models import User
+from data.models import User, News
 import os
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'role']
+
+
+class NewsSerializer(serializers.ModelSerializer):
+    poster = UserSerializer(read_only=True)
+    class Meta:
+        model = News
+        fields = ['id', 'poster', 'title', 'content', 'created_at']
