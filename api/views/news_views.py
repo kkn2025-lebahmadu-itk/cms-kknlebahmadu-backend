f2eba6b8dc740ebd50554ca7a96b5be2a037a062
rionando <rionandosoeksin@gmail.com>
2025-04-03 21:57:05 +0800

    feat: remove thumbnail from storage  after updating and deleting news

--- api/views/news_views.py
+++ api/views/news_views.py
@@ -9,7 +9,9 @@ from api.serializers import NewsSerializer, UserSerializer, GetNewsSerializer
 from data.permissions import NewsPermissions
 
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 
+import os
 
 
 
@@ -60,6 +62,10 @@ def news_views(request, id=None):
     elif request.method == 'PUT':
         data = request.data
         news = get_object_or_404(News, id=id)
+        if 'thumbnail' in data and news.thumbnail:
+            old_image_path = os.path.join(settings.MEDIA_ROOT, str(news.thumbnail))
+            if os.path.exists(old_image_path):
+                os.remove(old_image_path)
         serializer = NewsSerializer(news, data=data)
         if serializer.is_valid():
             news = serializer.save()
@@ -74,96 +80,13 @@ def news_views(request, id=None):
 
     elif request.method == 'DELETE':
         news = get_object_or_404(News, id=id)
+        if news.thumbnail:
+            image_path = os.path.join(settings.MEDIA_ROOT, str(news.thumbnail))
+            if os.path.exists(image_path):
+                os.remove(image_path)
         news.delete()
 
         response['messae'] = f'Berita dengan id {id} berhasil dihapus'
         
         return Response(response, status=status.HTTP_204_NO_CONTENT)
-
-
-# @api_view(['POST'])
-# @permission_classes([AdminOnly])
-# @parser_classes([MultiPartParser, FormParser])
-# def create_news(request):
-#     response = {}
-
-#     data = request.data.copy()
-#     data['poster'] = request.user.id
-    
-#     print(data)
-
-#     serializer = NewsSerializer(data=data, context={'request': request})
-#     if serializer.is_valid():
-#         print(serializer.validated_data)
-#         news = serializer.save()
-#         news.poster = request.user
-#         news.save()
-#         response['message'] = 'Berita baru sudah berhasil terbuat'
-#         response['data'] = {
-#             'title': news.title,
-#             'content': news.content,
-#             'poster': news.poster.username
-#         }
-#         return Response(response, status=status.HTTP_201_CREATED)
-
-#     response['message'] = 'Something went wrong'
-#     response['error'] = serializer.errors
-#     return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-# @api_view(['GET'])
-# @permission_classes([AllowAny])
-# def get_news(request, id=None):
-#     response = {}
-
-#     # fetch 1 berita aja kalo ada id di url
-#     if id:
-#         news = get_object_or_404(News, id=id)
-#         data = GetNewsSerializer(news, context={'request': request}).data
-#         response['message'] = f'Berita dengan id {id} berhasil difetch dari database'
-#         response['data'] = data
-#         return Response(response, status=status.HTTP_200_OK)
-    
-#     # Kalo nggak ada, fetch semua berita
-#     all_news = News.objects.all()
-#     data = GetNewsSerializer(all_news, many=True, context={'request': request}).data
-#     response['message'] = f'Semua berita berhasil difetch dari database'
-#     response['data'] = data
-#     return Response(response, status=status.HTTP_200_OK)
-
-
-
-# @api_view(['PUT'])
-# @permission_classes([AdminOnly])
-# def update_news(request, id):
-#     response = {}
-#     data = request.data
-#     news = get_object_or_404(News, id=id)
-#     serializer = NewsSerializer(news, data=data)
-#     if serializer.is_valid():
-#         news = serializer.save()
-#         response['message'] = f'Berita dengan id {id} berhasil diupdate'
-#         response['data'] = NewsSerializer(news).data
-#         return Response(response, status=status.HTTP_200_OK)
-#     else:
-#         response['message'] = 'Terjadi kesalahan'
-#         response['error'] = serializer.errors
-#         return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-
-# @api_view(['DELETE'])
-# @permission_classes([AdminOnly])
-# def delete_news(request, id):
-#     response = {}
-#     news = get_object_or_404(News, id=id)
-#     news.delete()
-
-#     response['messae'] = f'Berita dengan id {id} berhasil dihapus'
-    
-#     return Response(response, status=status.HTTP_204_NO_CONTENT)
-
-
-
 
