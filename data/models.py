76cecebb61f3ac65936a6e4f6e1c52931667f2c0
rionando <rionandosoeksin@gmail.com>
2025-05-11 07:45:55 +0800

    fix: pdf field

--- data/models.py
+++ data/models.py
@@ -74,7 +74,8 @@ class Gallery(models.Model):
     
 
 class Report(models.Model):
-    pdf = models.TextField(null=True, blank=True)
+    pdf = models.FileField(upload_to='report/', null=True, blank=True)
+    is_signed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
