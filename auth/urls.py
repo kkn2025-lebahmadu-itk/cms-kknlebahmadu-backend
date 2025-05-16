e365de312535f9aa02b81e51129b92ec79ff1ec6
rionando <rionandosoeksin@gmail.com>
2025-05-16 21:55:17 +0800

    feate: add routes to get currently authenticated user info

--- auth/urls.py
+++ auth/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register', views.register, name='register'),
+    path('user', views.get_user, name='get_user'),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from data.models import User
 
@@ -18,6 +18,27 @@ def register(request):
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
         response['message'] = "pengguna berhasil terdaftar"
+        return Response(response)
+    except Exception as e:
+        print(e)
+        response['message'] = "Terjadi kesalahan"
+        response['data'] = str(e)
+        return Response(response)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    response = {}
+    
+    try:
+        user = request.user
+        response['message'] = "Berhasil mendapatkan data pengguna"
+        response['user'] = {
+            'id': user.id,
+            'name': user.username,
+            'role': user.role
+        }
         return Response(response)
     except Exception as e:
         print(e)
