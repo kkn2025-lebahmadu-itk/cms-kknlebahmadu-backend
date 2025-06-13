3ccee774fb8480a90f1298de4671b58092ce40c0
rionando <rionandosoeksin@gmail.com>
2025-06-13 23:42:53 +0800

    feat: add seeder and fix cors

--- data/management/commands/seed.py
+++ data/management/commands/seed.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from data.models import Profile
 
 class Command(BaseCommand):
     help = "Seed the database with a superuser"
@@ -7,7 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
 
-        if not User.objects.filter(email="admin@example.com").exists():
+        if not User.objects.filter(email="superuser@gmail.com").exists():
             user = User(
                 username="Superuser",
                 email="superuser@gmail.com",
@@ -21,3 +22,39 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS("Superuser created successfully!"))
         else:
             self.stdout.write(self.style.WARNING("Superuser already exists."))
+
+        
+        if not User.objects.filter(email="admin@gmail.com").exists():
+            user = User(
+                username="Admin 1",
+                email="admin@gmail.com",
+                is_staff=True,
+                is_superuser=True,
+                role='admin'
+            )
+            user.set_password("admin321")
+            user.save()
+
+            self.stdout.write(self.style.SUCCESS("Superuser created successfully!"))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists."))
+
+        
+        profile_data = [
+            {"key": "Alamat", "value": "Telaga Sari, Balikpapan Kota", "type": "inti"},
+            {"key": "Nama Ketua RT", "value": "Gandung Kriswanto", "type": "inti"},
+            {"key": "Kegiatan Rutin", "value": "Kerja Bakti setiap hari sabtu", "type": "tambahan"},
+        ]
+
+        for item in profile_data:
+            obj, created = Profile.objects.get_or_create(
+                key=item["key"],
+                defaults={
+                    "value": item["value"],
+                    "type": item["type"],
+                }
+            )
+            if created:
+                self.stdout.write(self.style.SUCCESS(f"Profile '{item['key']}' created."))
+            else:
+                self.stdout.write(self.style.WARNING(f"Profile '{item['key']}' already exists."))
