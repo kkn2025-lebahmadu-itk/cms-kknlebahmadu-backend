437b4a4a2a48845df144c7445236928370727ba5
rionando <rionandosoeksin@gmail.com>
2025-06-13 22:27:55 +0800

    add change password

--- auth/urls.py
+++ auth/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('user', views.get_user, name='get_user'),
+    path('change-password', views.change_password, name='change_password'),
 ]
--- auth/views.py
+++ auth/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from data.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 @api_view(['POST'])
@@ -24,6 +25,38 @@ def register(request):
         response['message'] = "Terjadi kesalahan"
         response['data'] = str(e)
         return Response(response)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    response = {}
+
+    email = request.user.email
+    old_password = request.data.get('old_password')
+    new_password = request.data.get('new_password')
+
+    if not old_password or not new_password:
+        response['message'] = "Password lama dan baru harus diisi"
+        return Response(response, status=400)
+
+    user = authenticate(email=email, password=old_password)
+
+    if user is None:
+        response['message'] = "Password lama salah"
+        return Response(response, status=400)
+
+    try:
+        user.set_password(new_password)
+        user.save()
+
+        response['message'] = "Password berhasil diubah"
+        return Response(response)
+    except Exception as e:
+        print(e)
+        response['message'] = "Terjadi kesalahan"
+        response['data'] = str(e)
+        return Response(response, status=500)
 
 
 @api_view(['POST'])
